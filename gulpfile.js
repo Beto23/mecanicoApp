a8faff44c99632e222771f1ca7aa745f7c1f744d
Beto23 <josee-45@hotmail.com>
2015-09-25 13:41:08 -0500

    Estructura e index

--- gulpfile.js
+++ gulpfile.js
@@ -19,15 +19,19 @@ var jade = require('gulp-jade')
 
 
 gulp.task('js', function() {
-    return gulp.src("lib/*.js")
+    return gulp.src("lib/js/*.js")
     .pipe(uglify())
     .pipe(gulp.dest('./public/js'))// en dónde va a estar el archivo destino
 	.pipe(livereload())
 });
-
+gulp.task('js-vendor', function() {
+    return gulp.src("lib/js/vendor/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js/vendor'))// en dónde va a estar el archivo destino
+});
 
 gulp.task('styl', function() {
-  return gulp.src('./lib/*.styl') // entry point de styl
+  return gulp.src('./lib/styl/*.styl') // entry point de styl
     .pipe(stylus({ 
       use: nib(),
       'include css': true
@@ -39,21 +43,28 @@ gulp.task('styl', function() {
 })
 
 gulp.task('jade-components', function () {
-  return gulp.src('./lib/*.jade')
+  return gulp.src('./lib/views/*.jade')
   .pipe(jade({
     pretty: true
   }))
   .pipe(gulp.dest('./public'))
   .pipe(livereload());
 });
+gulp.task('img', function () {
+  return gulp.src(['lib/img/*.jpg','lib/img/*.png'])
+  .pipe(gulp.dest('public/img'))
+  
+});
 
 gulp.task('watch', function(){
   livereload.listen()
-  gulp.watch('./lib/**/**/*.styl', ['styl'])
-  gulp.watch('./lib/**/**/*.css', ['styl'])
-  gulp.watch('./lib/**/**/*.jade', ['jade-components'])
-  gulp.watch('./lib/**/**/*.js', ['js'])
+  gulp.watch('./lib/styl/*.styl', ['styl'])
+  gulp.watch('./lib/styl/*.css', ['styl'])
+  gulp.watch('./lib/views/*.jade', ['jade-components'])
+  gulp.watch('./lib/js/*.js', ['js'])
+  gulp.watch(['lib/img/*.jpg','lib/img/*.png'],['img'])
+
 })
 
 
-gulp.task('default', ['styl','jade-components', 'js', 'watch']) 
+gulp.task('default', ['styl','jade-components', 'js','img', 'watch']) 
